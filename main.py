f6f5ed754f50bf211ffca3741434d50aa74f2110
ploznik <ploznik@campus.technion.ac.il>
2020-06-07 00:14:18 +0300

    Changing the dataset's structure.

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-from torchtext.vocab import Vocab
-import random
-from torch.utils.data.dataset import Dataset, TensorDataset
-from pathlib import Path
-from collections import Counter
 import numpy as np
 import torch
 import torch.nn as nn
@@ -10,124 +5,170 @@ import torch.nn.functional as F
 import torch.optim as optim
 import random
 from torch.utils.data.dataloader import DataLoader
-
-train_path = "Data/train.labeled"
-
-PAD_TOKEN = 0
-ROOT_TOKEN = 0
-WORD_VEC_LEN = 2
-TAG_VEC_LEN = 2
+from collections import defaultdict
+from torchtext.vocab import Vocab
+from torch.utils.data.dataset import Dataset, TensorDataset
+from pathlib import Path
+from collections import Counter
 
 
-def embedding(input_type, em_method='rand'):
+def get_vocabs(file_path):
     """
-    embed a string to a numeric vector representation.
-
+    Extract vocabs from given data sets. Return a words_dict and tags_dict.
     Args:
-        input_type (str): word/tag each with different properties.
-        em_method (str): type of embedding method to be used. # TODO: right now support only random, should add word2vec/glove
+        file_path: a path to the requested file # TODO: should use a paths list
 
     Returns:
-        The embedded vector.
+        words_dict, tags_dict: a dictionary - keys:words\tags, items: counts of appearances.
     """
-    if em_method == 'rand':
-        if input_type == 'word':
-            return [random.uniform(0, 1) for i in range(WORD_VEC_LEN)]
+    words_dict = defaultdict(int)
+    tags_dict = defaultdict(int)
+    word_tag_dict = defaultdict(int)  # TODO: redundant?
 
-        else:
-            return [random.uniform(0, 1) for i in range(TAG_VEC_LEN)]
+    with open(file_path) as f:
+        for line in f:
+            split_line = line.split('\t')
+            if len(split_line) == 1:  # the end of a sentence denotes by \n line.
+                continue
+            word, pos_tag = split_line[1], split_line[3]
+            words_dict[word] += 1
+            tags_dict[pos_tag] += 1
+            word_tag_dict[(word, pos_tag)] += 1
 
-class DataReader:
+    return words_dict, tags_dict
+
+
+class PosDataReader:
     """ Read the data from the requested file and hold it's components. """
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, words_dict, tags_dict):
         """
         Args:
             file_path (str): holds the path to the requested file.
+            words_dict, tags_dict: a dictionary - keys:words\tags, items: counts of appearances.
         """
-
         self.file_path = file_path
-        self.words_dict = {}
-        self.tags_dict = {}
-        self.word_tag_dict = {}
+        self.words_dict = words_dict
+        self.tags_dict = tags_dict
         self.sentences = []
         self.__readData__()
 
     def __readData__(self):
-        """ main reader function. """
+        """main reader function which also populates the class data structures"""
+        cur_sentence_word_tag = []
+        cur_sentence_labels = []
 
-        cur_sentence = []
         with open(self.file_path, 'r') as f:
             for line in f:
                 split_line = line.split('\t')
                 if len(split_line) == 1:  # the end of a sentence denotes by \n line.
-                    self.sentences.append(cur_sentence)
-                    cur_sentence = []
+                    self.sentences.append((cur_sentence_word_tag, cur_sentence_labels))
+                    cur_sentence_word_tag = []
+                    cur_sentence_labels = []
                     continue
-                within_idx, word, pos_tag, head = (split_line[0], split_line[1], split_line[3], split_line[6])
-                cur_sentence.append((within_idx, word, pos_tag, head))
-
-                # Creating dictionaries for identification
-                if word not in self.words_dict:
-                    self.words_dict[word] = embedding('word')
-                if pos_tag not in self.tags_dict:
-                    self.tags_dict[pos_tag] = embedding('tag')
-                if (word, pos_tag) not in self.word_tag_dict:
-                    self.word_tag_dict[(word, pos_tag)] = self.words_dict[word] + self.tags_dict[pos_tag]
+                within_idx, word, pos_tag, head = (int(split_line[0]), split_line[1], split_line[3], int(split_line[6]))
+                cur_sentence_word_tag.append((word, pos_tag))
+                cur_sentence_labels.append((within_idx, head))
 
     def get_num_sentences(self):
-        """ returns the number of sentences in data. """
+        """returns num of sentences in data"""
         return len(self.sentences)
 
 
-class DependencyDataset(Dataset):
-    """ Holds version of our data as a PyTorch's Dataset object. """
+UNKNOWN_TOKEN = "<unk>"
+PAD_TOKEN = "<pad>"
+ROOT_TOKEN = "<root>"
+SPECIAL_TOKENS = [PAD_TOKEN, UNKNOWN_TOKEN]
 
-    def __init__(self, file_path: str, padding=True):
+
+class PosDataset(Dataset):
+    """
+    Holds version of our data as a PyTorch's Dataset object.
+    """
+    def __init__(self, words_dict, tags_dict, file_path, padding=False, word_embeddings=None):
         """
         Args:
             file_path (str): The path of the requested file.
             padding (bool): Gets true if padding is required.
+            word_embeddings: A set of words mapping.
         """
 
         super().__init__()
         self.file_path = file_path
-        self.data_reader = DataReader(self.file_path)
-        self.sentences = self.data_reader.sentences.copy()
-        self.sentence_lens = [len(sentence) for sentence in self.sentences]
+        self.data_reader = PosDataReader(self.file_path, word_dict, pos_dict)
+        self.vocab_size = len(self.data_reader.words_dict)
+        if word_embeddings:
+            self.words_idx_mappings, self.idx_words_mappings, self.words_vectors = word_embeddings
+        else:
+            self.words_idx_mappings, self.idx_words_mappings, self.words_vectors = self.init_word_embeddings(
+                self.data_reader.words_dict)
+            self.tags_idx_mappings, self.idx_tags_mappings, self.tags_vectors = self.init_tag_embeddings(
+                self.data_reader.tags_dict)
+        # self.labels_idx_mappings, self.idx_labels_mappings = self.init_labels_vocab(self.data_reader.tags_dict)
+
+        self.pad_idx = self.words_idx_mappings.get(PAD_TOKEN)
+        self.unknown_idx = self.words_idx_mappings.get(UNKNOWN_TOKEN)
+        # self.word_vector_dim = self.words_vectors.size(-1)
+        self.sentence_lens = [len(sentence[0]) for sentence in self.data_reader.sentences]
         self.max_seq_len = max(self.sentence_lens)
-        self.pre_processing()
-
-    def pre_processing(self):
-        # words_dict = self.data_reader.words_dict
-        # tags_dict = self.data_reader.tags_dict
-        word_tag_dict = self.data_reader.word_tag_dict
-
-        for i, sentence in enumerate(self.sentences):
-            words_part = [word_tag_dict[(token[1], token[2])] for token in sentence]
-            labels_part = [(int(token[0]), int(token[3])) for token in sentence]
-
-            # Padding:
-            while len(words_part) < self.max_seq_len:
-                words_part.append([PAD_TOKEN for j in range(WORD_VEC_LEN+TAG_VEC_LEN)])
-                labels_part.append((PAD_TOKEN, PAD_TOKEN))
-
-            self.sentences[i] = [torch.tensor(words_part), torch.tensor(labels_part)]
+        self.sentences_dataset = self.convert_sentences_to_dataset(padding)
 
     def __len__(self):
-        return len(self.sentences)
+        return len(self.sentences_dataset)
 
-    def __getitem__(self, idx):
-        word_embed_idx, labels = self.sentences[idx]
-        return word_embed_idx, labels
+    def __getitem__(self, index):
+        word_embed_idx, pos_embed_idx, sentence_len = self.sentences_dataset[index]
+        return word_embed_idx, pos_embed_idx, sentence_len
+
+    @staticmethod
+    def init_word_embeddings(words_dict):
+        glove = Vocab(Counter(words_dict), vectors=None, specials=SPECIAL_TOKENS)
+        return glove.stoi, glove.itos, glove.vectors
+
+    @staticmethod
+    def init_tag_embeddings(tags_dict):
+        glove = Vocab(Counter(tags_dict), vectors=None, specials=SPECIAL_TOKENS)
+        return glove.stoi, glove.itos, glove.vectors
+
+    def get_words_embeddings(self):
+        return self.words_idx_mappings, self.idx_words_mappings, self.words_vectors
+
+    def get_tags_embeddings(self):
+        return self.tags_idx_mappings, self.idx_tags_mappings, self.tags_vectors
+
+    def convert_sentences_to_dataset(self, padding):
+        sentence_word_tag_idx_list = list()
+        sentence_labels_idx_list = list()
+        sentence_len_list = list()
+        for sentence_idx, sentence in enumerate(self.data_reader.sentences):
+            words_tags_idx_list = []
+            labels_idx_list = []
+            for word_tag, labels in zip(sentence[0], sentence[1]):
+                words_tags_idx_list.append((self.words_idx_mappings.get(word_tag[0]),
+                                            self.tags_idx_mappings.get(word_tag[1])))
+                labels_idx_list.append(labels)
+            sentence_len = len(words_tags_idx_list)
+            if padding:
+                while len(words_tags_idx_list) < self.max_seq_len:
+                    words_tags_idx_list.append((self.words_idx_mappings.get(PAD_TOKEN), self.words_idx_mappings.get(PAD_TOKEN)))
+                    labels_idx_list.append((self.words_idx_mappings.get(PAD_TOKEN), self.words_idx_mappings.get(PAD_TOKEN)))
+            sentence_word_tag_idx_list.append(words_tags_idx_list)
+            sentence_labels_idx_list.append(labels_idx_list)
+            sentence_len_list.append(sentence_len)
+
+        if padding:
+            all_sentence_word_tag_idx = torch.tensor(sentence_word_tag_idx_list, dtype=torch.long)
+            all_sentence_labels_idx = torch.tensor(sentence_labels_idx_list, dtype=torch.long)
+            all_sentence_len = torch.tensor(sentence_len_list, dtype=torch.long, requires_grad=False)
+            return TensorDataset(all_sentence_word_tag_idx, all_sentence_labels_idx, all_sentence_len)
 
 
 if __name__ == '__main__':
-    dataset = DependencyDataset("Data/train.labeled")
-    train_loader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=2)
+    path_train = "Data/train.labeled"
 
-    for i, images in enumerate(train_loader):
-        print(images)
-
-        if i == 0:
-            break
+    word_dict, pos_dict = get_vocabs(path_train)
+    train = PosDataset(word_dict, pos_dict, path_train, padding=True)
+    train_data_loader = DataLoader(train, batch_size=2, shuffle=True)
+    for i, data in enumerate(train_data_loader):
+        print(data)
+        break
